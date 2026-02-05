d642de9a985f57a0c8411d65b6a2924498742cfd
Wave Dev <66224387+wavedevgit@users.noreply.github.com>
2026-02-05 18:32:01 +0100

    fix endpoints issues

--- getEndpoints.js
+++ getEndpoints.js
@@ -1,60 +1,95 @@
-import * as acorn from "acorn";
-import * as walk from "acorn-walk"
+import * as acorn from 'acorn';
+import * as walk from 'acorn-walk';
+
+function parseTemplate(functionBody, name) {
+    const templates = [];
+    if (!functionBody) {
+        console.log('Got no function body??');
+        return '';
+    }
+    walk.simple(functionBody, {
+        TemplateLiteral(node) {
+            templates.push(node);
+        },
+    });
+    if (templates.length > 1)
+        console.log(
+            name,
+            'Found property with more then 1 template literals in string, it might cause issues',
+        );
+
+    const template = templates.splice(-1)[0];
+    let path = '';
+    for (let i = 0; i < template.quasis.length; i++) {
+        path += template.quasis[i].value.raw;
+
+        if (i < template.expressions.length) {
+            // skip repeating stuff on same place
+            if (!path.endsWith('/:param') && !path.endsWith('=:param'))
+                path +=
+                    path.endsWith('/') || path.endsWith('=')
+                        ? ':param'
+                        : '/:param';
+        }
+    }
+
+    return path;
+}
 
 export default function getEndpoints(file) {
-	const endpoints = {}
+    const endpoints = {};
 
-	const ast = acorn.parse(file, {ecmaVersion: "latest"})
-	walk.simple(ast, {
-		ObjectExpression(node, state) {
-			if(state.step === 0 && node.properties.find(x => x.key?.name === "USER_RELATIONSHIP")) {
-				state.step = 1
+    const ast = acorn.parse(file, { ecmaVersion: 'latest' });
+    walk.simple(
+        ast,
+        {
+            ObjectExpression(node, state) {
+                if (
+                    state.step === 0 &&
+                    node.properties.find(
+                        (x) => x.key?.name === 'USER_RELATIONSHIP',
+                    )
+                ) {
+                    state.step = 1;
 
-				for(const property of node.properties) {
-					const key = property.key.name
-					let path
+                    for (const property of node.properties) {
+                        const key = property.key.name;
+                        let path;
 
-					if(property.value.type === "Literal") {
-						path = property.value.value
-					} else if(["FunctionExpression", "ArrowFunctionExpression"].includes(property.value.type)) {
-						path = ""
+                        if (property.value.type === 'Literal') {
+                            path = property.value.value;
+                        }
 
-						if(property.value.type === "ArrowFunctionExpression" && property.value.body.type === "Literal") {
-							path = property.value.body.value
-						} else if(property.value.type === "ArrowFunctionExpression" && property.value.body.type === "TemplateLiteral") {
-							// Handle template literals like `/guilds/${e}/profile/${t}`
-							const templateLiteral = property.value.body
-							for(let i = 0; i < templateLiteral.quasis.length; i++) {
-								path += templateLiteral.quasis[i].value.raw
-								if(i < templateLiteral.expressions.length) {
-									path += ":param"
-								}
-							}
-						} else {
-							walk.simple(property.value, {
-								CallExpression(node) {
-									if(node.callee.type === "MemberExpression" && node.callee.object.type === "Literal") {
-										path += node.callee.object.value
-									}
-									for(const argument of node.arguments) {
-										if(argument.type === "Identifier") {
-											path += ":param"
-										} else if(argument.type === "Literal") {
-											path += argument.value
-										}
-									}
-								}
-							})
-						}
-					} else {
-						throw new Error(`Unexpected node type: ${property.value.type}`)
-					}
+                        if (
+                            !path &&
+                            ![
+                                'FunctionExpression',
+                                'ArrowFunctionExpression',
+                            ].includes(property.value.type)
+                        )
+                            throw new Error(
+                                `Unexpected node type: ${property.value.type}`,
+                            );
 
-					endpoints[key] = path
-				}
-			}
-		}
-	}, undefined, {step: 0})
+                        if (
+                            property.value.type === 'ArrowFunctionExpression' &&
+                            property.value.body.type === 'Literal'
+                        )
+                            path = property.value.body.value;
+                        if (!path)
+                            path = parseTemplate(
+                                property.value.body,
+                                property.key.name,
+                            );
 
-	return endpoints
+                        endpoints[key] = path;
+                    }
+                }
+            },
+        },
+        undefined,
+        { step: 0 },
+    );
+
+    return endpoints;
 }
